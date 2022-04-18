07d6a5afe229eeb4cbf273eb68b0b4a7445ca9f1
Ruslan <sonikx94@yandex.ru>
2022-04-18 19:52:30 +0300

    Задание 2

--- Lesson8/Lesson8.cpp
+++ Lesson8/Lesson8.cpp
@@ -26,6 +26,52 @@ namespace lesson8 {
         return a / b;
     }
 
+    /*
+     * 2. Написать класс Ex, хранящий вещественное число x и имеющий конструктор по вещественному числу,
+     * инициализирующий x значением параметра. Написать класс Bar, хранящий вещественное число y
+     * (конструктор по умолчанию инициализирует его нулем) и имеющий метод set с единственным вещественным параметром a.
+     * Если y + a > 100, возбуждается исключение типа Ex с данными a*y, иначе в y заносится значение a.
+     * В функции main завести переменную класса Bar и в цикле в блоке try вводить с клавиатуры целое n.
+     * Использовать его в качестве параметра метода set до тех пор, пока не будет введено 0.
+     * В обработчике исключения выводить сообщение об ошибке, содержащее данные объекта исключения.
+     */
+
+    class Ex : public std::logic_error {
+    private:
+        float x;
+
+    public:
+        Ex(const float x) : std::logic_error("Error!") {
+            this->x = x;
+        }
+
+        float getX() const {
+            return x;
+        }
+
+        const char* what() const noexcept override {
+            return std::logic_error::what();
+        }
+    };
+
+    class Bar {
+    private:
+        float y;
+
+    public:
+        Bar() {
+            y = 0.0f;
+        }
+
+        void set(float a) {
+            if (y + a > 100) {
+                throw Ex(a*y);
+            }
+
+            y = a;
+        }
+    };
+
     // ================ Тестирование ================
 
     void divTest() {
@@ -51,9 +97,31 @@ namespace lesson8 {
         }
     }
 
+    void barTest() {
+        Bar b;
+
+        int n = -1;
+        while (n != 0) {
+            std::cout << "Please, enter n:" << std::endl;
+            std::cin >> n;
+
+            try {
+                if (n != 0)
+                    b.set(float(n));
+            } catch (const Ex& e) {
+                std::cout << e.what() << " Error number = " << e.getX() << std::endl;
+            }
+        }
+    }
+
     void run() {
         std::cout << "====================== LESSON 8 ======================" << std::endl;
         std::cout << "Task 1:" << std::endl;
         divTest();
+
+        std::cout << std::endl;
+
+        std::cout << "Task 2:" << std::endl;
+        barTest();
     }
 }
